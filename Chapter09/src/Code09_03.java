d9e986d212caed0a1a5771d38a48cd48df1a40cf
zz8086 <167443710+zz8086@users.noreply.github.com>
2024-10-30 09:38:41 +0900

    Update Code09_03.java

--- Chapter09/src/Code09_03.java
+++ Chapter09/src/Code09_03.java
@@ -9,11 +9,16 @@ public class Code09_03 {
 		
 		for (int i = 0; i <= 3; i++)
 		{
-			System.out.print("숫자 : ");
+			System.out.print("numAry[" + i + "]의 넣을 숫자 : ");
 			numAry[i] = s.nextInt();
 		}
 		
-		hap = numAry[0] +numAry[1] + numAry[2] + numAry[3];
+		
+		for (int i = 0; i <= 3; i++)
+		{
+			hap += numAry[i];
+		}
+		
 		System.out.println("합계 ==> " + hap);
 		
 		s.close();
