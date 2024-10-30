45b8a537deab5f9ec275625aba1580ebb91be5bb
zz8086 <167443710+zz8086@users.noreply.github.com>
2024-10-30 10:27:51 +0900

    Add files via upload

---
+++ Chapter09/src/Code09_07.java
@@ -0,0 +1,20 @@
+
+public class Code09_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ary = {10, 20, 30};
+		int hap;
+		hap = 0;
+		
+		for (int data : ary)
+		{
+			hap += data;
+			System.out.println(data);
+			System.out.println(hap);
+		}
+
+		
+	}
+
+}
