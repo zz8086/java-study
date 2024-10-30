00881d164a1e970c18ae2d43780f06bd436b50b5
zz8086 <167443710+zz8086@users.noreply.github.com>
2024-10-30 09:32:29 +0900

    Add files via upload

---
+++ Chapter09/src/Code09_01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Code09_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int num1, num2, num3, num4;
+		int hap = 0;
+		
+		System.out.print("숫자 : ");
+		num1 = s.nextInt();
+		System.out.print("숫자 : ");
+		num2 = s.nextInt();
+		System.out.print("숫자 : ");
+		num3 = s.nextInt();
+		System.out.print("숫자 : ");
+		num4 = s.nextInt();
+		
+		hap = num1 + num2 + num3 + num4;
+		System.out.println("합계 ==> " + hap);
+		
+		s.close();
+				
+	}
+
+}
